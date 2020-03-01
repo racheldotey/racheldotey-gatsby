0d20cdfced33dd34b85614e74f94990cf67202d6
Rachel Dotey <rachel.dotey@gmail.com>
2020-03-01 10:23:49 -0500

    Added built with section to the footer.
    Added a 'this site is built with' section to the footer and created components for the images to be loaded with gatsby-image. This needs to be refactored in the future as I do not believe it is very efficient.

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,16 +1,50 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { Container, Row, Col } from "react-bootstrap"
 
+import LogoCSS3 from "../images/logo-css3"
+import LogoGatsbyJS from "../images/logo-gatsbyjs"
+import LogoGraphql from "../images/logo-graphql"
+import LogoHTML5 from "../images/logo-html5"
+import LogoNodeJS from "../images/logo-nodejs"
+import LogoReactJS from "../images/logo-reactjs"
+import LogoSASS from "../images/logo-sass"
 import './footer.scss'
 
 const Footer = ({ siteTitle }) => (
   <footer class="footer">
-    <div class="container">
-      <p>© 2012-{new Date().getFullYear()} <Link to="#contact">Contact</Link><a href="https://www.racheldotey.com/about/contact">Rachel Dotey</a> All Rights Reserved. | <a href="/policies/">Website Policies</a></p>
+    <Container>
+      <Row>
+        <Col>
+          <p>This website built with <a href="https://www.nodejs.org">Node</a>, React and <a href="https://www.gatsbyjs.org">Gatsby</a> on Netifly.com.</p>
+        </Col>
+        <Col>
+          <h4>Location</h4>
+          <p>Working with small businesses across New England. Based in Burlington Vermont.</p>
+        </Col>
+        <Col>
+          <h4>Connect with Rachel </h4>
+          <p>607-386-5872<br />
+          <Link to="/contact">hello{'{'}at{'}'}racheldotey.com</Link></p>
+        </Col>
+      </Row>
+    </Container>
+    <div className="footer-built-with">
+      <small>Built with</small><br/>
+      <LogoHTML5 height="20px" width="20px" /><LogoCSS3/><LogoSASS/><LogoNodeJS/><LogoReactJS/><LogoGatsbyJS/><LogoGraphql/>
     </div>
-    <div class="container">
-      <p>This website built with <a href="https://www.nodejs.org">Node</a>, React and <a href="https://www.gatsbyjs.org">Gatsby</a> on Netifly.com.</p>
+    <div className="footer-copy">
+      <Container>
+        <Row>
+          <Col>
+          <p>© 2012-{new Date().getFullYear()} <Link to="/contact">Rachel Dotey</Link> All rights reserved.</p>
+          </Col>
+          <Col>
+          <p className="text-right"><Link to="/site-map">Site Map</Link> | <Link to="/policies">Website Policies</Link></p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   </footer>
 )
---
+++ src/components/images/logo-sass.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+export default () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "logo_sass_256.png" }) {
+        childImageSharp {
+          fixed(width: 56) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+
+  return <Img fixed={data.placeholderImage.childImageSharp.fixed} />
+}
