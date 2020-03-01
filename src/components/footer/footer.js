9c4c13fa63748948512c2f9e980a98072d056591
Rachel Dotey <rachel.dotey@gmail.com>
2020-03-01 11:40:03 -0500

    Footer layout.

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -10,38 +10,54 @@ import LogoHTML5 from "../images/logo-html5"
 import LogoNodeJS from "../images/logo-nodejs"
 import LogoReactJS from "../images/logo-reactjs"
 import LogoSASS from "../images/logo-sass"
+import Signature from "../images/signature-black"
 import './footer.scss'
 
 const Footer = ({ siteTitle }) => (
   <footer class="footer">
-    <Container>
-      <Row>
-        <Col>
-          <p>This website built with <a href="https://www.nodejs.org">Node</a>, React and <a href="https://www.gatsbyjs.org">Gatsby</a> on Netifly.com.</p>
-        </Col>
-        <Col>
-          <h4>Location</h4>
-          <p>Working with small businesses across New England. Based in Burlington Vermont.</p>
-        </Col>
-        <Col>
-          <h4>Connect with Rachel </h4>
-          <p>607-386-5872<br />
-          <Link to="/contact">hello{'{'}at{'}'}racheldotey.com</Link></p>
-        </Col>
-      </Row>
-    </Container>
-    <div className="footer-built-with">
-      <small>Built with</small><br/>
-      <LogoHTML5 height="20px" width="20px" /><LogoCSS3/><LogoSASS/><LogoNodeJS/><LogoReactJS/><LogoGatsbyJS/><LogoGraphql/>
-    </div>
-    <div className="footer-copy">
+    <div className="footer-main">
       <Container>
         <Row>
           <Col>
-          <p>© 2012-{new Date().getFullYear()} <Link to="/contact">Rachel Dotey</Link> All rights reserved.</p>
+            <h4>How can I make<br />
+              <span className="highlight">your business better</span>?</h4>
+
+            <div class="buttons">
+              <a title="I would love to talk about your business!" href="contact" class="button button-size-large"><span>Lets Talk</span><i class="fal fa-long-arrow-right" aria-hidden="true"></i></a>
+
+            </div>
+
           </Col>
           <Col>
-          <p className="text-right"><Link to="/site-map">Site Map</Link> | <Link to="/policies">Website Policies</Link></p>
+            <h4>Location</h4>
+            <p>Working with small businesses across New England. Based in Burlington Vermont.</p>
+          </Col>
+          <Col>
+            <h4>Connect with Rachel </h4>
+            <p><a title="Call me at 607-386-5872" href="tel:607-386-5872">607-386-5872</a><br />
+              <Link title="Lets discuss your business!" to="/contact">hello{'{'}at{'}'}racheldotey.com</Link></p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+    <div className="footer-built-with">
+      <small>Built with</small><br />
+      <LogoHTML5 height="20px" width="20px" /><LogoCSS3 /><LogoSASS /><LogoNodeJS /><LogoReactJS /><LogoGatsbyJS /><LogoGraphql />
+    </div>
+    <div className="footer-copywrite">
+      <Container>
+        <Row>
+          <Col>
+            <Link title={"© 2012-" + new Date().getFullYear() + " Rachel L Dotey, Website and Software Developer"} to="/contact"><Signature /></Link>
+          </Col>
+          <Col>
+            <div className="d-flex h-100 text-right">
+              <div className="justify-content-center align-self-center ml-auto">
+                <span>
+                  <Link to="/site-map">Site Map</Link> | <Link to="/policies">Website Policies</Link>
+                </span>
+              </div>
+            </div>
           </Col>
         </Row>
       </Container>
---
+++ src/components/images/signature-black.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+export default () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "signature_236x64.png" }) {
+        childImageSharp {
+          fixed(width: 236) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+
+  return <Img fixed={data.placeholderImage.childImageSharp.fixed} />
+}
