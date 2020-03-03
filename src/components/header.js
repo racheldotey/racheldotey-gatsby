bc17bb2bee376962640a10993347a11a076db623
Rachel Dotey <rachel.dotey@gmail.com>
2020-03-03 07:02:23 -0500

    Menu updates to test on mobile.

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,4 @@
+import Link from "gatsby-link"
 import PropTypes from "prop-types"
 import React from "react"
 
@@ -15,9 +16,9 @@ const Header = ({ siteTitle }) => (
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto"></Nav>
           <Nav className="nav-main-menu">
-            <Nav.Link to="/about">About</Nav.Link>
-            <Nav.Link eventKey={2} to="/services">Services</Nav.Link>
-            <Nav.Link eventKey={3} to="/contact">Contact</Nav.Link>
+            <Link to="/about"  className="nav-link">About</Link>
+            <Link eventKey={2} to="/services" className="nav-link">Services</Link>
+            <Link eventKey={3} to="/contact" className="nav-link">Contact</Link>
           </Nav>
           <Nav className="smallview-nav-contact d-sm-none">
             <div>
