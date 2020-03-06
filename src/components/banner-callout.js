9c6ede07efbe8f0a864f28389be863a730d5f535
Rachel Dotey <rachel.dotey@gmail.com>
2020-03-06 12:11:06 -0500

    Banner responsive tweaks
    Reordering columns on responsive banner

--- src/components/banner-callout.js
+++ src/components/banner-callout.js
@@ -20,19 +20,19 @@ const CallOutBanner = () => {
     <section class="callout-banner">
       <Container>
         <Row className="d-flex h-100 py-3 mb-xs-3">
-          <Col sm={12}  md={4}  lg={6} className="align-self-center text-center pb-3">
-            <div className="profile-image">
-              <Link className="image-link" to="/contact">
-                <Img style={{ "max-height": "340px", "max-width": "340px" }} fluid={data.imageSign.childImageSharp.fluid} />
-              </Link>
-            </div>
-          </Col>
           <Col sm={12}  md={8}  lg={6} className="order-md-first">
             <h2>Lets talk about what will take your business presence to the next level.</h2>
             <p className="lead">Whether its a website redesign, a targeted local search campaign, or developing a new application to provide your service to users in an innovative way - I'm the developer you are looking for.</p>
             <p className="lead">Reach out today.</p>
             <div className="pt-3 text-center">
               <Link className="button button-light" to="/contact">Connect with me</Link>
+            </div>
+          </Col>
+          <Col sm={12}  md={4}  lg={6} className="align-self-center text-center py-3">
+            <div className="profile-image">
+              <Link className="image-link" to="/contact">
+                <Img style={{ "max-height": "340px", "max-width": "340px" }} fluid={data.imageSign.childImageSharp.fluid} />
+              </Link>
             </div>
           </Col>
         </Row>
