aa7d07c6eb613605caf89f53fc4d63fd435037fc
Rachel Dotey <rachel.dotey@gmail.com>
2020-03-17 20:50:34 -0400

    Added noopener to links
    Added noopener to links to prevent vulnerability. Noreferrer for backward compatibility.

--- src/components/images/built-with-logos.js
+++ src/components/images/built-with-logos.js
@@ -76,29 +76,29 @@ export default () => {
 
   return (
     <>
-      <a title="HTML5 is the latest evolution of the standard that defines HTML." href="https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5" target="_blank">
+      <a title="HTML5 is the latest evolution of the standard that defines HTML." href="https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5" target="_blank" rel="noopener noreferrer">
         <Img fixed={data.logoHTML.childImageSharp.fixed} />
       </a>
-      <a title="CSS3 is the latest evolution of the Cascading Style Sheets language." href="https://developer.mozilla.org/en-US/docs/Archive/CSS3" target="_blank">
+      <a title="CSS3 is the latest evolution of the Cascading Style Sheets language." href="https://developer.mozilla.org/en-US/docs/Archive/CSS3" target="_blank" rel="noopener noreferrer">
         <Img fixed={data.logoCSS.childImageSharp.fixed} />
       </a>
-      <a title="Sass is the most mature, stable, and powerful professional grade CSS extension language in the world." href="https://sass-lang.com/" target="_blank">
+      <a title="Sass is the most mature, stable, and powerful professional grade CSS extension language in the world." href="https://sass-lang.com/" target="_blank" rel="noopener noreferrer">
         <Img fixed={data.logoSASS.childImageSharp.fixed} />
       </a>
       <br className="d-block d-sm-none" />
-      <a title="Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine." href="https://nodejs.org/" target="_blank">
+      <a title="Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine." href="https://nodejs.org/" target="_blank" rel="noopener noreferrer">
         <Img fixed={data.logoNodeJS.childImageSharp.fixed} />
       </a>
-      <a title="React.js is a JavaScript library for building user interfaces." href="https://reactjs.org/" target="_blank">
+      <a title="React.js is a JavaScript library for building user interfaces." href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
         <Img fixed={data.logoReactJS.childImageSharp.fixed} />
       </a>
-      <a title="Gatsby.js is a free and open source framework based on React that helps developers build blazing fast websites and apps." href="https://gatsbyjs.org/" target="_blank">
+      <a title="Gatsby.js is a free and open source framework based on React that helps developers build blazing fast websites and apps." href="https://gatsbyjs.org/" target="_blank" rel="noopener noreferrer">
         <Img fixed={data.logoGatsbyJS.childImageSharp.fixed} />
       </a>
-      <a title="GraphQL is a query language for APIs and a runtime for fulfilling those queries with your existing data." href="https://graphql.org/" target="_blank">
+      <a title="GraphQL is a query language for APIs and a runtime for fulfilling those queries with your existing data." href="https://graphql.org/" target="_blank" rel="noopener noreferrer">
         <Img fixed={data.logoGraphql.childImageSharp.fixed} />
       </a>
-      <a title="This bad mama jama website is hosted on Netlify" href="https://www.netlify.com/" target="_blank">
+      <a title="This bad mama jama website is hosted on Netlify" href="https://www.netlify.com/" target="_blank" rel="noopener noreferrer">
         <Img fixed={data.logoNetlify.childImageSharp.fixed} />
       </a>
     </>
--- src/components/images/follow-my-social.js
+++ src/components/images/follow-my-social.js
@@ -36,14 +36,14 @@ export default () => {
           }
         }
       }
-      iconGithub: file(relativePath: { eq: "icon_social_github.png" }) {
+      iconLinkedIn: file(relativePath: { eq: "icon_social_linkedin.png" }) {
         childImageSharp {
           fixed(width: 32, height: 32) {
             ...GatsbyImageSharpFixed
           }
         }
       }
-      iconLinkedIn: file(relativePath: { eq: "icon_social_linkedin.png" }) {
+      iconGit: file(relativePath: { eq: "icon_social_github.png" }) {
         childImageSharp {
           fixed(width: 32, height: 32) {
             ...GatsbyImageSharpFixed
@@ -55,17 +55,20 @@ export default () => {
 
   return (
     <div className="follow-my-social">
-      <a title="View my Instagram" href="https://instagram.com/dancingrabbit/" target="_blank" className="pr-2">
+      <a title="Fork my Github" href="https://github.com/racheldotey" target="_blank" rel="noopener noreferrer" className="pr-2">
+        <Img fixed={data.iconGit.childImageSharp.fixed} />
+      </a>
+      <a title="View my Instagram" href="https://instagram.com/dancingrabbit" target="_blank" rel="noopener noreferrer" className="pr-2">
         <Img fixed={data.iconInstagram.childImageSharp.fixed} />
       </a>
-      <a title="Follow me on Twitter" href="https://twitter.com/RaeDeveloper" target="_blank" className="pr-2">
+      <a title="Follow me on Twitter" href="https://twitter.com/RaeDeveloper" target="_blank" rel="noopener noreferrer" className="pr-2">
         <Img fixed={data.iconTwitter.childImageSharp.fixed} />
       </a>
-      <a title="Read my LinkedIn" href="https://www.linkedin.com/in/racheldotey/" target="_blank" className="pr-2">
-        <Img fixed={data.iconLinkedIn.childImageSharp.fixed} />
+      <a title="View my Facebook" href="https://www.facebook.com/rachelldotey" target="_blank" rel="noopener noreferrer" className="pr-2">
+        <Img fixed={data.iconFacebook.childImageSharp.fixed} />
       </a>
-      <a title="Fork my Github" href="https://github.com/racheldotey" target="_blank">
-        <Img fixed={data.iconGithub.childImageSharp.fixed} />
+      <a title="Read my LinkedIn" href="https://www.linkedin.com/in/racheldotey" target="_blank" rel="noopener noreferrer">
+        <Img fixed={data.iconLinkedIn.childImageSharp.fixed} />
       </a>
     </div>
   )
