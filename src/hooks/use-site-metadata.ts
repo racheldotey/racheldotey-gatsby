fa3cd27057c0c8acf7e0adced1ed9f8520c01c18
Rachel Dotey <rachel.dotey@gmail.com>
2023-04-11 19:31:24 -0400

    Update use-site-metadata.ts

--- src/hooks/use-site-metadata.ts
+++ src/hooks/use-site-metadata.ts
@@ -1,19 +1,37 @@
 import { useStaticQuery, graphql } from 'gatsby';
 
-export const useSiteMetadata = () => {
+export type SiteMetadataType = {
+  author: {
+    name: string,
+    bio: string,
+  };
+  email: string;
+  phone: string;
+  tagline: string;
+  title: string;
+};
+
+export const useSiteMetadata = (): SiteMetadataType => {
   const { site } = useStaticQuery(
     graphql`
       query SiteMetaData {
         site {
           siteMetadata {
-            title
-            tagline
-            phone
+            author {
+              name
+              bio
+            }
             email
+            phone
+            tagline
+            title
           }
         }
       }
     `
   );
+
   return site.siteMetadata;
 };
+
+export default useSiteMetadata;
