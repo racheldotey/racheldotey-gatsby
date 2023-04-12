e8adbb4a2394510297fa1d72ecb98332890d5dfe
Rachel Dotey <rachel.dotey@gmail.com>
2023-04-12 10:39:41 -0400

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -12,8 +12,79 @@ import CallOutBanner from '../components/banner-callout';
 import SocialButtons from '../components/follow-my-social';
 import Seo from '../components/seo';
 
-//https://www.canva.com/templates/EADaoa2YgA8-pink-blue-design-resume/
-const AboutPage: React.FC<PageProps> = () => {
+const AboutTextSection: React.FC = () => {
+  return (
+    <>
+      <p>
+        Hello! I'm Rachel, a{' '}
+        <a target="_blank" href="/humans.txt" title="humans.txt">
+          carbon-based life form
+        </a>{' '}
+        who has been building websites since the{' '}
+        <a
+          target="_blank"
+          href="https://www.webhamster.com/"
+          title="The Hamster Dance"
+        >
+          90s
+        </a>
+        . That's right, I am one of the internet's original{' '}
+        <a
+          target="_blank"
+          href="https://thehistoryoftheweb.com/postscript/what-happened-to-the-webmaster/"
+          title="What Happened to the Webmaster - The History of the Web"
+        >
+          webmasters
+        </a>
+        . My skills were forged in the arena of{' '}
+        <a
+          target="_blank"
+          href="https://wiki.archiveteam.org/index.php/GeoCities"
+          title="GeoCities - Archiveteam"
+        >
+          GeoCities
+        </a>{' '}
+        before{' '}
+        <a
+          target="_blank"
+          href="https://content.time.com/time/business/article/0,8599,1936645,00.html"
+          title="Yahoo! Pulls Plug on GeoCities, Erases Internet History - TIME"
+        >
+          the fall
+        </a>{' '}
+        of the ancient digital world. My first version of JavaScript was v1.5 on{' '}
+        <a
+          target="_blank"
+          href="https://en.wikipedia.org/wiki/Netscape_Navigator"
+          title="Netscape Navigator - Wikipedia"
+        >
+          Netscape Navigator
+        </a>{' '}
+        circa 1999.
+      </p>
+      <p>
+        Since then, I have evolved into a full-stack software developer with
+        expertise in a wide range of technologies. My skills span across
+        front-end and back-end software development, project management, search
+        engine optimization, and graphic design. Additionally, I have experience
+        working with the latest JavaScript frameworks such as React, Angular,
+        and Vue.
+      </p>
+      <p>
+        I specialize in building responsive and accessible websites that provide
+        a seamless user experience. I use Node.js to develop single-page
+        applications (SPA) and progressive web apps (PWA), as well as
+        implementing web components and animations.
+      </p>
+      <p>
+        My goal is to provide the best possible results for my clients by using
+        the most suitable tools and frameworks for each project.
+      </p>
+    </>
+  );
+};
+
+const ImageSection: React.FC = () => {
   const { author } = useSiteMetadata();
   const { phone, email } = author;
 
@@ -23,61 +94,46 @@ const AboutPage: React.FC<PageProps> = () => {
     "Let's build something great together! Contact me now to discuss your software development needs.";
 
   return (
+    <>
+      <div className="profile-image">
+        <StaticImage
+          src="../images/picture-square.png"
+          alt="This is the developer you are looking for..."
+          placeholder="blurred"
+          layout="fixed"
+          width={236}
+        />
+      </div>
+      <p>
+        <a title={phoneCTA} href={`tel:${phone}`}>
+          {phone}
+        </a>
+        <br />
+        <Link title={emailCTA} to="/contact">
+          {email}
+        </Link>
+      </p>
+      <SocialButtons />
+    </>
+  );
+};
+
+const AboutPage: React.FC<PageProps> = () => {
+  return (
     <Layout>
       <Container className="my-5">
         <Row className="justify-content-md-center">
           <Col md={12} lg={10} className="text-center page-intro">
-            <div className="inline-image">
-              <StaticImage
-                src="../images/signature_236x64.png"
-                alt="This is the developer you are looking for..."
-                placeholder="blurred"
-                layout="fixed"
-                width={236}
-              />
-            </div>
-            <p className="lead">
-              Web Design, Software Development, and Search Engine Marketing
-            </p>
+            <h1 className="title">About Me</h1>
+            <p className="lead">Webmaster from the 90's</p>
           </Col>
         </Row>
         <Row className="pt-4">
           <Col sm={12} md={6}>
-            <p>
-              I am a full stack developer with over 10 years of experience
-              across a broad range of technologies.
-            </p>
-            <p>
-              My background includes front-end and back-end software
-              development, project management, search engine optimization,and
-              marketing.
-            </p>
-            <p>
-              I love working with fun things like React and WordPress, and I
-              actually know quite a few languages and frameworks. I like to use
-              the best tool for the job to get the best results.
-            </p>
+            <AboutTextSection />
           </Col>
           <Col sm={12} md={6} className="text-center">
-            <div className="profile-image">
-              <StaticImage
-                src="../images/picture-square.png"
-                alt="This is the developer you are looking for..."
-                placeholder="blurred"
-                layout="fixed"
-                width={236}
-              />
-            </div>
-            <p>
-              <a title={phoneCTA} href={`tel:${phone}`}>
-                {phone}
-              </a>
-              <br />
-              <Link title={emailCTA} to="/contact">
-                {email}
-              </Link>
-            </p>
-            <SocialButtons />
+            <ImageSection />
           </Col>
         </Row>
       </Container>
